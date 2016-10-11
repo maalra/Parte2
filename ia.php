57aa2f1bbf61772e8161aac6c02a34c29aa6bc43
Marc Alapont Raga <maalra01@floridauniversitaria.es>
2016-10-11 13:16:36 +0200

    first commit

---
+++ ia.php
@@ -0,0 +1,25 @@
+<?php
+class ia
+{
+//declaracion de una propiedad
+public $columna = '1';
+public $fila = '3';
+//declaracion de metodo
+public function getcolumna(){
+return $this->columna;
+}
+public function getfila(){
+return $this->fila;
+}
+public function setColumna($col){
+$this->columna=$col;
+}
+public function setFila($fil){
+$this->fila=$fil;
+}
+
+//randoms
+public function randomPos(){
+}
+}
+?>
